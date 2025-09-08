d2f7a6a682e1134f15098845069c6d0e8b742ee5
Dominik Weikert <dominik.weikert@ovgu.de>
2025-09-08 12:29:55 +0200

    force osc52

--- lua/options.lua
+++ lua/options.lua
@@ -79,3 +79,22 @@ vim.opt.confirm = true
 
 vim.opt.bg = 'dark'
 vim.opt.termguicolors = true
+
+-- Make sure Neovim doesn't try to use remote xclip/xsel
+vim.g.clipboard = nil
+
+-- Don't force unnamed/unnamedplus globally
+vim.opt.clipboard = {}
+
+-- force osc52
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+    ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+    ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+  },
+}
